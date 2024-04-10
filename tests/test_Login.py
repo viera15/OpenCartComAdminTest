314fe7fa1a9488955cef62e2698de63070ca1fef
Viera <vierarajnakova@gmail.com>
2024-04-10 15:24:04 +0200

    vytvorený prvý nástrel testu bez funkcie

--- tests/test_Login.py
+++ tests/test_Login.py
@@ -1,10 +1,12 @@
 import time
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get("https://demo.opencart.com/admin/")
 
-time.sleep(3)
-driver.quit()
+driver.find_element(By.ID, "input-username").send_keys("demo")
+driver.find_element(By.ID,"input-password").send_keys("demo")
+driver.find_element(By.XPATH, "//button[contains(text(), ' Login')]").click()
