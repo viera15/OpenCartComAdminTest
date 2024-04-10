93b8469020b32f8e9105193c66734388634de0c3
Viera <vierarajnakova@gmail.com>
2024-04-10 16:31:24 +0200

    prvá verzia testu prihlásenie so správnymi údajmi vo funkcii - test_Login.py

--- tests/test_Login.py
+++ tests/test_Login.py
@@ -5,21 +5,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
-driver = webdriver.Firefox()
-driver.maximize_window()
-driver.get("https://demo.opencart.com/admin/")
+def test_login_spravne_udaje():
+    driver = webdriver.Firefox()
+    driver.maximize_window()
+    driver.get("https://demo.opencart.com/admin/")
 
-driver.find_element(By.ID, "input-username").send_keys("demo")
-driver.find_element(By.ID,"input-password").send_keys("demo")
-driver.find_element(By.XPATH, "//button[contains(text(), 'Login')]").click()
+    driver.find_element(By.ID, "input-username").send_keys("demo")
+    driver.find_element(By.ID,"input-password").send_keys("demo")
+    driver.find_element(By.XPATH, "//button[contains(text(), 'Login')]").click()
 
-wait = WebDriverWait(driver, 35)
-wait.until(expected_conditions.visibility_of_element_located((By.XPATH, "//div[@class='modal-dialog']")))
-driver.find_element(By.XPATH, "//*[@id='modal-security']/div/div/div[1]/button").click()
+    wait = WebDriverWait(driver, 35)
+    wait.until(expected_conditions.visibility_of_element_located((By.XPATH, "//div[@class='modal-dialog']")))
+    driver.find_element(By.XPATH, "//*[@id='modal-security']/div/div/div[1]/button").click()
 
-ocakavany_text = "Logout"
-assert driver.find_element(By.XPATH, "//header/div/ul/li[3]/a/span").text.__contains__(ocakavany_text)
+    ocakavany_text = "Logout"
+    assert driver.find_element(By.XPATH, "//header/div/ul/li[3]/a/span").text.__contains__(ocakavany_text)
 
 
-time.sleep(5)
-driver.quit()
+    time.sleep(5)
+    driver.quit()
