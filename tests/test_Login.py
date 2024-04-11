d3aee3f94f92f8608e7821fb38e26630e2671ff8
Viera <vierarajnakova@gmail.com>
2024-04-11 14:04:15 +0200

    zjednodušenie testu správneho prihlásenia test_Login.py

--- tests/test_Login.py
+++ tests/test_Login.py
@@ -8,9 +8,7 @@ from tests.BaseTest import BaseTest
 class TestLogin(BaseTest):
 
     def test_login_spravne_udaje(self):
-        admin_page = AdminPage(self.driver)
-        dashboard_page = admin_page.prihlasenie_admina("demo", "demo")
-        dashboard_page.zatvorenie_modalneho_okna()
+        dashboard_page = self.login_spravne_udaje()
         ocakavany_text = "Logout"
         assert dashboard_page.vratenie_textu_logout().__contains__(ocakavany_text)
 
