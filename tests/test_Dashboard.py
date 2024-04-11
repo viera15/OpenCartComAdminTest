b1413dc9be989241d2fb070831bc44eee90394e2
Viera <vierarajnakova@gmail.com>
2024-04-11 11:42:18 +0200

    vytvorená hlavička test_Dashboard.py

---
+++ tests/test_Dashboard.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+
+from tests.BaseTest import BaseTest
+
+
+
+class TestDashboard(BaseTest):
+
+    def __init__(self, driver):
+        super().__init__(driver)
+
